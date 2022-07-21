3e52a894b50acc49585e701460564b7de43666fc
xutu <dawn_tx@163.com>
2022-07-21 16:40:44 +0800

    1

---
+++ cv-shardingsphere-sample/src/main/java/com/clearvision/ShardingsphereSampleApplication.java
@@ -0,0 +1,17 @@
+package com.clearvision;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan("com.clearvision")
+@MapperScan("com.clearvision.mybatissample.mapper")
+public class ShardingsphereSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingsphereSampleApplication.class, args);
+    }
+
+}
