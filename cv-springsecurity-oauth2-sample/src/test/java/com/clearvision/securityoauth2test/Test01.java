fcd71d6fa4f988a99a928ae6d3a3c332e2222489
xutu <dawn_tx@163.com>
2022-08-01 17:10:35 +0800

    security基础代码提交

--- cv-springsecurity-oauth2-sample/src/test/java/com/clearvision/securityoauth2test/Test01.java
+++ cv-springsecurity-oauth2-sample/src/test/java/com/clearvision/securityoauth2test/Test01.java
@@ -1,9 +1,11 @@
 package com.clearvision.securityoauth2test;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.keygen.BytesKeyGenerator;
+import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.security.crypto.password.*;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
@@ -16,17 +18,36 @@ public class Test01 {
     @Test
     public void test02(){
 
-        // 1. delegate encode
-        String idForEncode = "bcrypt";
-        Map encoders = new HashMap<>();
-        encoders.put(idForEncode, new BCryptPasswordEncoder());
-        encoders.put("noop", NoOpPasswordEncoder.getInstance());
-        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
-        encoders.put("scrypt", new SCryptPasswordEncoder());
-        encoders.put("sha256", new StandardPasswordEncoder());
 
-        PasswordEncoder passwordEncoder =
-                new DelegatingPasswordEncoder(idForEncode, encoders);
+
+
+        // Create an encoder with strength 16
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+        String result = encoder.encode("myPassword");
+        boolean myPassword = encoder.matches("myPassword1", result);
+        if (myPassword){
+            System.out.println("1111111");
+        }
+
+
+        BytesKeyGenerator generator = KeyGenerators.secureRandom();
+        byte[] key = generator.generateKey();
+
+        System.out.println();
+
+
+
+        // 1. delegate encode
+//        String idForEncode = "bcrypt";
+//        Map encoders = new HashMap<>();
+//        encoders.put(idForEncode, new BCryptPasswordEncoder());
+//        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+//        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
+//        encoders.put("scrypt", new SCryptPasswordEncoder());
+//        encoders.put("sha256", new StandardPasswordEncoder());
+//
+//        PasswordEncoder passwordEncoder =
+//                new DelegatingPasswordEncoder(idForEncode, encoders);
 
 //        User user = new User();
 
