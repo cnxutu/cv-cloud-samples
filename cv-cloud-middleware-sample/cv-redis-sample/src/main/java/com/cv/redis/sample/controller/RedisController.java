1d3f79592e6cd9e2d315cdea999d57309b2eb931
xutu <cnxutu@gmail.com>
2024-07-05 09:13:24 +0800

    fix(redis-jedis): 测试集成完成

--- cv-cloud-middleware-sample/cv-redis-sample/src/main/java/com/cv/redis/sample/controller/RedisController.java
+++ cv-cloud-middleware-sample/cv-redis-sample/src/main/java/com/cv/redis/sample/controller/RedisController.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -14,6 +15,7 @@ import javax.annotation.Resource;
  * @author: xutu
  * @since: 2024/7/3 17:07
  */
+@RestController
 @RequestMapping("/sample/redis")
 public class RedisController {
 
