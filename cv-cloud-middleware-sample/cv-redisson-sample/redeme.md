9bfea6c4ea94fd15b295bf50d089ee9688de4dee
xutu <cnxutu@gmail.com>
2025-03-29 17:19:20 +0800

    refactor(redisson): 调整包结构
    1

---
+++ cv-cloud-middleware-sample/cv-redisson-sample/redeme.md
@@ -0,0 +1,5 @@
+
+
+
+## 官方文档
+http://redisson.pro/docs/
--- cv-cloud-middleware-sample/cv-redisson-sample/src/main/java/com/cv/redisson/ResissonApplication.java
+++ cv-cloud-middleware-sample/cv-redisson-sample/src/main/java/com/cv/redisson/ResissonApplication.java
@@ -1,4 +1,4 @@
-package com.cv;
+package com.cv.redisson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
