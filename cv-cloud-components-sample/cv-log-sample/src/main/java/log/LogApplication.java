e070c3980194220a8561246b5e543495d64c24ae
xutu <cnxutu@gmail.com>
2025-04-11 10:01:51 +0800

    feat(新增模块): 新增日志模块
    1

---
+++ cv-cloud-components-sample/cv-log-sample/src/main/java/log/LogApplication.java
@@ -0,0 +1,15 @@
+package log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogApplication.class, args);
+    }
+
+}
