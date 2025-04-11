f5859a357936e9a7e78efc8462012bf2a7a089ff
xutu <cnxutu@gmail.com>
2025-04-11 13:57:43 +0800

    fix(启动类): 添加 mapper 扫描注解1
    1

--- cv-cloud-components-sample/cv-log-sample/src/main/java/com/cv/log/LogApplication.java
+++ cv-cloud-components-sample/cv-log-sample/src/main/java/com/cv/log/LogApplication.java
@@ -1,11 +1,13 @@
 package com.cv.log;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.cv.log.sample.dao")
 public class LogApplication {
 
     public static void main(String[] args) {
